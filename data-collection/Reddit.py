0bc54c979bcdd0594844263cb0fdd4f00c3c1d14
AdminL3 <l-blu@outlook.de>
2024-10-26 19:48:12 +0200

    added reddit api access into reddit file
    to remove pycache

--- data-collection/Reddit.py
+++ data-collection/Reddit.py
@@ -1,6 +1,8 @@
 import config
-
-reddit = config.reddit
+import praw
+reddit = praw.Reddit(client_id=config.client_id,
+                     client_secret=config.client_secret,
+                     user_agent=config.user_agent)
 subreddit = reddit.subreddit('PrisonBreak')
 for post in subreddit.hot(limit=5):
     print(f'Title: {post.title}, Score: {post.score}')
