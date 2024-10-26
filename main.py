32825a36d66bef9da3ba74133f3db994eaece307
AdminL3 <l-blu@outlook.de>
2024-10-26 13:06:29 +0200

    Big update --> Check description
    I added the option to use an existing chrome session so X doesnt become suspicios of multiple logins. i developed this in a different file and put it in a if else statement

--- main.py
+++ main.py
@@ -1,66 +1,94 @@
 import time
+import pyperclip
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+
+print("Do you want to login to existing session? (y/n)")
+
+while True:
+    try:
+        x = input().strip().lower()
+        if x == "y":
+            x = True
+            break
+        elif x == "n":
+            x = False
+            break
+        else:
+            raise ValueError("Invalid input, please enter 'y' or 'n'.")
+    except ValueError as e:
+        print(e)
 
 
-options = webdriver.ChromeOptions()
-options.add_argument("--start-maximized")
-options.add_argument("--disable-search-engine-choice-screen")
-options.add_experimental_option("detach", True)
-driver = webdriver.Chrome(options=options)
+if x:
+    print('You are expected to have all prior steps done as explained in the README!')
+    pyperclip.copy(
+        '"C:\Program Files\Google\Chrome\Application\chrome.exe" --remote-debugging-port=9222')
+    print('Copied command to clipboard')
+    input("Press any key to continue...")
+    options = Options()
+    options.add_experimental_option("debuggerAddress", "localhost:9222")
+    driver = webdriver.Chrome(options=options)
 
 
-driver.get('https://x.com/home')
-print(driver.title)
+else:
+    options = webdriver.ChromeOptions()
+    options.add_argument("--start-maximized")
+    options.add_argument("--disable-search-engine-choice-screen")
+    options.add_experimental_option("detach", True)
+    driver = webdriver.Chrome(options=options)
 
-time.sleep(3)
+    driver.get('https://x.com/home')
+    print(driver.title)
 
+    time.sleep(3)
 
-# accept all cookies
-button = driver.find_element(
-    By.XPATH, "//button[.//span[text()='Alle Cookies akzeptieren']]")
-button.click()
+    # accept all cookies
+    button = driver.find_element(
+        By.XPATH, "//button[.//span[text()='Alle Cookies akzeptieren']]")
+    button.click()
 
+    # login
+    driver.get('https://x.com/signin')
+    user = "LeviBlu412024"
+    password = "X+aDGi@S484+qcL"
+    email = "l-blu@outlook.de"
 
-# login
-driver.get('https://x.com/signin')
-user = "LeviBlu412024"
-password = "X+aDGi@S484+qcL"
-email = "l-blu@outlook.de"
+    time.sleep(3)
 
-time.sleep(3)
+    # benutzername
+    username = driver.find_element(By.TAG_NAME, 'input')
+    username.send_keys(user)
 
-# benutzername
-username = driver.find_element(By.TAG_NAME, 'input')
-username.send_keys(user)
-
-# click enter
-driver.find_element(By.XPATH, "//span[text()='Weiter']").click()
-time.sleep(2)
-
-inputs = driver.find_elements(By.TAG_NAME, 'input')
-try:
-    verification = driver.find_element(
-        By.XPATH, "//span[text()='Gib deine Telefonnummer oder E-Mail-Adresse ein']")
-    inputs[-1].send_keys(email)
+    # click enter
     driver.find_element(By.XPATH, "//span[text()='Weiter']").click()
-except:
-    pass
+    time.sleep(2)
 
-time.sleep(1)
-inputs = driver.find_elements(By.TAG_NAME, 'input')
-# passwort
-inputs[-1].send_keys(password)
+    inputs = driver.find_elements(By.TAG_NAME, 'input')
+    try:
+        verification = driver.find_element(
+            By.XPATH, "//span[text()='Gib deine Telefonnummer oder E-Mail-Adresse ein']")
+        inputs[-1].send_keys(email)
+        driver.find_element(By.XPATH, "//span[text()='Weiter']").click()
+    except:
+        pass
 
-# click login
-driver.find_element(By.XPATH, "//span[text()='Anmelden']").click()
-time.sleep(2)
+    time.sleep(1)
+    inputs = driver.find_elements(By.TAG_NAME, 'input')
+    # passwort
+    inputs[-1].send_keys(password)
 
-# go home
-driver.get('https://x.com/home')
+    # click login
+    driver.find_element(By.XPATH, "//span[text()='Anmelden']").click()
+    time.sleep(2)
+
+    # go home
+
+    print("Successfully logged in!")
 
 print("Successfully logged in!")
-
+driver.get('https://x.com/home')
 # search function
 print("What would you like to search for?")
 base_url = "https://x.com/search?q={}&src=typed_query&f=top"
