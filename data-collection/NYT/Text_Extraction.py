571ea54dd99e769266088295895b0155a4952e39
AdminL3 <l-blu@outlook.de>
2024-10-28 09:48:46 +0100

    open selenium driver

--- data-collection/NYT/Text_Extraction.py
+++ data-collection/NYT/Text_Extraction.py
@@ -5,3 +5,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from bs4 import BeautifulSoup
 
+# Setup Chrome options
+options = webdriver.ChromeOptions()
+options.add_argument("--start-maximized")
+options.add_argument("--disable-search-engine-choice-screen")
+options.add_experimental_option("detach", True) 
+options.add_argument("--headless")
+driver = webdriver.Chrome(options=options)
+
+# Go to Google to initialize driver
+driver.get('https://google.com/')
+print(driver.title)
