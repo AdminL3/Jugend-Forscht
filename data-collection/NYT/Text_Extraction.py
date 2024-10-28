3ce4aee653683f93c2ef39218520fcd652c20a50
AdminL3 <l-blu@outlook.de>
2024-10-28 09:48:27 +0100

    imports

--- data-collection/NYT/Text_Extraction.py
+++ data-collection/NYT/Text_Extraction.py
@@ -1,1 +1,7 @@
+import os
+import pyperclip
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
+from bs4 import BeautifulSoup
 
