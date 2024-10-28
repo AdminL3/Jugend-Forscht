12f4cb442c1649d7d6447c8321452fdf5d217bfd
AdminL3 <l-blu@outlook.de>
2024-10-28 09:46:11 +0100

    check if file already exists

--- data-collection/NYT/Links.py
+++ data-collection/NYT/Links.py
@@ -8,3 +8,11 @@ year = 2020
 month = 0
 topics = ["us", "politics", "world"]
 API_KEY = config.NYT_API_KEY
+
+for month_idx in range(12):
+    month += 1
+    file_path = f"data/NYT/links/{topics[0]}/month{month}.txt"
+    URL = f"https://api.nytimes.com/svc/archive/v1/{year}/{month}.json?api-key={API_KEY}"
+    if os.path.exists(file_path):
+        print(f"File 'month{month}.txt'  already exists. Skipping...")
+        continue
