6faa3a42ee5db62222bda6ca3d31b5873f6f1543
AdminL3 <l-blu@outlook.de>
2024-10-29 22:11:39 +0100

    moved files --> updated gitignore

--- data-collection/Reddit.py
+++
@@ -1,8 +0,0 @@
-import config
-import praw
-reddit = praw.Reddit(client_id=config.client_id,
-                     client_secret=config.client_secret,
-                     user_agent=config.user_agent)
-subreddit = reddit.subreddit('PrisonBreak')
-for post in subreddit.hot(limit=5):
-    print(f'Title: {post.title}, Score: {post.score}')
