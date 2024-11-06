f384c727015d88e07dfe358be924db9a204fc299
AdminL3 <l-blu@outlook.de>
2024-11-06 16:08:45 +0100

    updated from selenium to requests

--- data-collection/Source_Extraction_Requests.py
+++ data-collection/Source_Extraction_Requests.py
@@ -7,20 +7,19 @@ headers = config.headers
 
 
 
-start_year = 2020
+start_year = 2021
 amount_years = 1
 topics = ["world"]
-start_month = 2
+start_month = 12
 amount_month = 1
 last_date = 0
-driver = webdriver.Chrome(options=options)
 for topic in topics:
     for i in range(amount_years):
         year = start_year + i
         for i in range(amount_month):
             month = start_month + i
             print(f"Next Month: {month}")
-            urls_path = f"/content/urls/{year}/month{month}.txt"
+            urls_path = f"data/NYT/links/{topic}/{year}/month{month}.txt"
             # urls_path = f"/content/urls/month1.txt"
             with open(urls_path, 'r', encoding='utf-8') as file:
                 urls = file.read().splitlines()
@@ -37,21 +36,25 @@ for topic in topics:
                     index = 1
                 file_name = f"{date}{index}.txt"
 
-                output_dir = f"/content/data/NYT/source/{topic}/{year}/month{month}/"
+                output_dir = f"data/NYT/source/{topic}/{year}/month{month}/"
                 os.makedirs(output_dir, exist_ok=True)
                 output_file = os.path.join(output_dir, file_name)
                 if os.path.exists(output_file):
                     print(f"File {file_name} already exists. Skipping...")
                     continue
 
-                driver.get(url)
+                try:
+                    response = requests.get(url)
+                    page_source = response.text
+                    with open(output_file, "w", encoding="utf-8") as f:
+                        f.write(page_source)
+                        print(page_source)
+                        print(f"Success {file_name}")
+                        
+                        
+                except requests.exceptions.RequestException as e:
+                    print(f"Error fetching webpage: {e}")
                 time.sleep(1)
-                page_source = driver.execute_script("return document.documentElement.outerHTML;")
-
-                with open(output_file, "w", encoding="utf-8") as f:
-                    f.write(page_source)
-                    print(f"Success {file_name}")
-
 
                 last_date = date
                 
