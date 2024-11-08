6f7f8c180d916fc2a6b0103c9e38a8727cdcf8da
AdminL3 <l-blu@outlook.de>
2024-11-08 15:11:17 +0100

    removing corresponding source files

--- data-collection/other/empty_files.py
+++ data-collection/other/empty_files.py
@@ -1,19 +1,22 @@
 import os
 
-# run againg for empty folders
 
 base = r'C:\Users\L-Blu\Levi\Programmieren\Python\Jugend-Forscht\data\articles'
+base2 = 'C:\\Users\\L-Blu\\Levi\\Programmieren\\Python\\Jugend-Forscht\\data\\source\\'
 
 
-for dirpath, dirnames, filenames in os.walk(base, topdown=False):
-    # Check if the directory is empty
-    if not os.listdir(dirpath):  # If the folder is empty
-        print(f"Deleting empty folder: {dirpath}")
-        os.rmdir(dirpath)  # Delete the empty folder
+for _ in range(2):
+    for dirpath, dirnames, filenames in os.walk(base, topdown=False):
+        # Check if the directory is empty
+        if not os.listdir(dirpath):  # If the folder is empty
+            print(f"Deleting empty folder: {dirpath}")
+            os.rmdir(dirpath)  # Delete the empty folder
 
-    # Also check for empty files and delete them
-    for file in filenames:
-        file_path = os.path.join(dirpath, file)
-        if os.path.getsize(file_path) == 0:  # If the file is empty
-            print(f"Deleting empty file: {file_path}")
-            os.remove(file_path)
+        # Also check for empty files and delete them
+        for file in filenames:
+            file_path = os.path.join(dirpath, file)
+            if os.path.getsize(file_path) == 0:  # If the file is empty
+                print(f"Deleting empty file: {file_path}")
+                os.remove(file_path)
+                os.remove(os.path.join(
+                    base2 + os.path.join('\\'.join(file_path.split('\\')[9:14]))))
