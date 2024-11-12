de08ea57fd7b838987ca0c36322be264a2699ac1
AdminL3 <l-blu@outlook.de>
2024-11-12 18:40:15 +0100

    Working Step 2

--- Analysing/Wordcount/Graph.py
+++ Analysing/Wordcount/Graph.py
@@ -1,10 +1,11 @@
 import sqlite3
 import matplotlib.pyplot as plt
 import pandas as pd
-import numpy as np
+
 
 connection = sqlite3.connect("Analysing\Wordcount\wordcount.db")
 cursor = connection.cursor()
+
 
 cursor.execute("SELECT * FROM Wordcount;")
 rows = cursor.fetchall()
@@ -12,22 +13,20 @@ rows = cursor.fetchall()
 
 Dataframe = pd.DataFrame(
     rows, columns=[column[0] for column in cursor.description])
-Dataframe = Dataframe.drop(columns=['id'])
 
+
+Dataframe = Dataframe.drop(columns=['id'])
 
 Dataframe['date'] = pd.to_datetime(Dataframe['date'])
 Dataframe.set_index('date', inplace=True)
 
-ax = Dataframe.plot(style='o', markersize=2)
-
+Dataframe.plot(style='o', markersize=2)
 
 plt.xlabel("Date")
 plt.ylabel("")
-
+plt.legend()
+plt.title("Word Count Analysis")
 plt.legend(['Wordcount'])
-
-
 plt.savefig("Analysing\Wordcount\docs\img2.png")
-plt.show()
 
-connection.close()
+plt.show()
