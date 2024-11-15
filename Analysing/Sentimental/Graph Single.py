f8aa4e2d9e509150bbf1b0f225db932fad0364c7
AdminL3 <l-blu@outlook.de>
2024-11-15 15:39:00 +0100

    Copied from wordcount

---
+++ Analysing/Sentimental/Graph Single.py
@@ -0,0 +1,41 @@
+import sqlite3
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LinearRegression
+
+connection = sqlite3.connect("Analysing\Wordcount\wordcount.db")
+cursor = connection.cursor()
+
+
+cursor.execute("SELECT * FROM Politics;")
+rows = cursor.fetchall()
+
+
+Dataframe = pd.DataFrame(
+    rows, columns=[column[0] for column in cursor.description])
+
+
+Dataframe = Dataframe.drop(columns=['id'])
+
+Dataframe['date'] = pd.to_datetime(Dataframe['date'])
+Dataframe.set_index('date', inplace=True)
+
+Dataframe.plot(style='o', markersize=2)
+
+
+X = Dataframe.index.astype(np.int64).values.reshape(-1, 1)
+y = Dataframe['wordcount']
+model = LinearRegression()
+model.fit(X, y)
+y_pred = model.predict(X)
+plt.plot(Dataframe.index, y_pred, color='red')
+
+
+plt.xlabel("Date")
+plt.ylabel("")
+plt.legend(["Word Count", "Regression Line"])
+plt.title("Word Count Analysis")
+plt.savefig("Analysing\Wordcount\docs\img4.png")
+
+plt.show()
