85d8454a480fe78eb1f4c99f3ff5c6248c705e73
AdminL3 <l-blu@outlook.de>
2024-11-16 15:35:25 +0100

    indented everything for loop

--- Analysing/Articlecount/Graph Months.py
+++ Analysing/Articlecount/Graph Months.py
@@ -5,42 +5,44 @@ import pandas as pd
 connection = sqlite3.connect("Analysing/Articlecount/articlecount.db")
 cursor = connection.cursor()
 
-topic_id = 0
 
-topics = ["Politics", "World"]
-topic = topics[topic_id]
-colors = ['#1f77b4', '#ff7f0e']
-color = colors[0] if topic == "Politics" else colors[1]
+for i in range(2):
+    topic_id = i
 
+    topics = ["Politics", "World"]
+    topic = topics[topic_id]
+    colors = ['#1f77b4', '#ff7f0e']
+    color = colors[0] if topic == "Politics" else colors[1]
 
-cursor.execute(f"SELECT * FROM monthly_totals WHERE topic_id = {topic_id}")
-rows = cursor.fetchall()
+    cursor.execute(f"SELECT * FROM monthly_totals WHERE topic_id = {topic_id}")
+    rows = cursor.fetchall()
 
-# Create DataFrame
-Dataframe = pd.DataFrame(
-    rows, columns=[column[0] for column in cursor.description])
+    # Create DataFrame
+    Dataframe = pd.DataFrame(
+        rows, columns=[column[0] for column in cursor.description])
 
-# Drop unnecessary columns
-Dataframe = Dataframe.drop(columns=['id'])
-Dataframe = Dataframe.drop(columns=['topic_id'])
+    # Drop unnecessary columns
+    Dataframe = Dataframe.drop(columns=['id'])
+    Dataframe = Dataframe.drop(columns=['topic_id'])
 
-# Combine 'year' and 'month' into a 'date' column
-Dataframe['date'] = pd.to_datetime(Dataframe[['year', 'month']].assign(day=1))
+    # Combine 'year' and 'month' into a 'date' column
+    Dataframe['date'] = pd.to_datetime(
+        Dataframe[['year', 'month']].assign(day=1))
 
-# Set 'date' as the index
-Dataframe.set_index('date', inplace=True)
+    # Set 'date' as the index
+    Dataframe.set_index('date', inplace=True)
 
-# Plot the article counts by month
-Dataframe.plot(y='total_count', style='o', markersize=4, color=color)
+    # Plot the article counts by month
+    Dataframe.plot(y='total_count', style='o', markersize=4, color=color)
 
-# Customize the plot
-plt.xlabel("Date")
-plt.ylabel("Article Count")
-plt.legend(["Article Count"])
-plt.title(f"Article Count Analysis for {topics[topic_id]}")
-plt.tight_layout()
+    # Customize the plot
+    plt.xlabel("Date")
+    plt.ylabel("Article Count")
+    plt.legend(["Article Count"])
+    plt.title(f"Article Count Analysis for {topics[topic_id]}")
+    plt.tight_layout()
 
-# Save the plot
-plt.savefig(f"Analysing/Articlecount/output/{topics[topic_id]}.png")
+    # Save the plot
+    plt.savefig(f"Analysing/Articlecount/output/Month/{topics[topic_id]}.png")
 
-plt.close()
+    plt.close()
