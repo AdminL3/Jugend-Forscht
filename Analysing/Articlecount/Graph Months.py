0e1b34d366575fad388ade71976a370276f915a8
AdminL3 <l-blu@outlook.de>
2024-11-17 10:23:00 +0100

    Adde regression

--- Analysing/Articlecount/Graph Months.py
+++ Analysing/Articlecount/Graph Months.py
@@ -1,18 +1,18 @@
 import sqlite3
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
+from sklearn.linear_model import LinearRegression
+
 
 connection = sqlite3.connect("Analysing/Articlecount/articlecount.db")
 cursor = connection.cursor()
 
-
-topics = ["Politics", "World"]
 colors = ['#1f77b4', '#ff7f0e']
-for topic_id in range(len(topics)):
-    topic = topics[topic_id]
-    color = colors[topic_id]
-
-    cursor.execute(f"SELECT * FROM monthly_totals WHERE topic_id = {topic_id}")
+colors_reg = ['blue', 'red']
+topics = ["Politics", "World"]
+for i, topic in enumerate(topics):
+    cursor.execute(f"SELECT * FROM monthly_totals WHERE topic_id = {i}")
     rows = cursor.fetchall()
 
     # Create DataFrame
@@ -31,12 +31,22 @@ for topic_id in range(len(topics)):
     Dataframe.set_index('date', inplace=True)
 
     # Plot the article counts by month
-    Dataframe.plot(y='total_count', style='o', markersize=4, color=color)
+    plt.plot(Dataframe.index,
+             Dataframe['total_count'], 'o', markersize=4, color=colors[i])
+
+    X = Dataframe.index.astype(np.int64).values.reshape(-1, 1)
+    y = Dataframe['total_count']
+    model = LinearRegression()
+    model.fit(X, y)
+    y_pred = model.predict(X)
+    plt.plot(Dataframe.index, y_pred, color=colors_reg[i])
 
     # Customize the plot
+    legend1 = [f"Article Count for {topic}"]
+    legend2 = [f"Regression Line for {topic}"]
     plt.xlabel("Date")
     plt.ylabel("Article Count")
-    plt.legend([f"Article Count for {topic}"])
+    plt.legend(legend1 + legend2)
     plt.title(f"Monthly Analysis of Amount of Articles for {topic}")
     plt.tight_layout()
 
@@ -68,12 +78,31 @@ for i in range(len(topics)):
     plt.plot(Dataframe.index,
              Dataframe['total_count'], 'o', markersize=4, color=f'{colors[i]}')
 
+for i, topic in enumerate(topics):
+    cursor.execute(f"SELECT * FROM monthly_totals WHERE topic_id = {i}")
+    rows = cursor.fetchall()
 
+    Dataframe = pd.DataFrame(
+        rows, columns=[column[0] for column in cursor.description])
+
+    Dataframe = Dataframe.drop(columns=['id'])
+    Dataframe = Dataframe.drop(columns=['topic_id'])
+
+    Dataframe['date'] = pd.to_datetime(
+        Dataframe[['year', 'month']].assign(day=1))
+
+    X = Dataframe.index.astype(np.int64).values.reshape(-1, 1)
+    y = Dataframe['total_count']
+    model = LinearRegression()
+    model.fit(X, y)
+    y_pred = model.predict(X)
+    plt.plot(Dataframe.index, y_pred, color=colors_reg[i])
 # Customize the plot
-legend = [f"Article Count for {topic}" for topic in topics]
+legend1 = [f"Article Count for {topic}" for topic in topics]
+legend2 = [f"Regression Line for {topic}" for topic in topics]
 plt.xlabel("Date")
 plt.ylabel("Article Count")
-plt.legend(legend)
+plt.legend(legend1 + legend2)
 plt.title(f"Monthly Analysis of Amount of Articles")
 plt.tight_layout()
 
