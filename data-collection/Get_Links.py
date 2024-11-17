d82178afed480ce10fbb4b5d4806d8e3b668e03d
AdminL3 <l-blu@outlook.de>
2024-11-17 11:29:30 +0100

    Better checks if files exists

--- data-collection/Get_Links.py
+++ data-collection/Get_Links.py
@@ -9,35 +9,34 @@ amount_years = 2
 
 topics = ["world", "politics", "opinion"]
 API_KEY = config.NYT_API_KEY
+for topic in topics:
+    for i in range(amount_years):
+        year = start_year + i
+        for month in range(1, 13):
+            file_exists = os.path.exists(
+                f"data/links/{topic}/{year}/month{month}.txt")
 
-for i in range(amount_years):
-    year = start_year + i
-    for month in range(1, 13):
-        all_files_exist = all(os.path.exists(
-            f"data/links/{topic}/{year}/month{month}.txt") for topic in topics)
+            if file_exists:
+                print(f"File for month {month} in year {
+                    year} already exist. Skipping...")
+                continue
 
-        if all_files_exist:
-            print(f"Files for month {month} in year {
-                  year} already exist. Skipping...")
-            continue
+            URL = f"https://api.nytimes.com/svc/archive/v1/{
+                year}/{month}.json?api-key={API_KEY}"
+            try:
+                # get data
+                response = requests.get(URL)
+                response.raise_for_status()
+                data = response.json()
+                articles = data.get("response", {}).get("docs", [])
+            except requests.exceptions.RequestException as e:
+                print(f"Error fetching data: {e}")
+                time.sleep(5)
+                continue
+            except ValueError:
+                print("Error parsing JSON response.")
+                continue
 
-        URL = f"https://api.nytimes.com/svc/archive/v1/{
-            year}/{month}.json?api-key={API_KEY}"
-        try:
-            # get data
-            response = requests.get(URL)
-            response.raise_for_status()
-            data = response.json()
-            articles = data.get("response", {}).get("docs", [])
-        except requests.exceptions.RequestException as e:
-            print(f"Error fetching data: {e}")
-            time.sleep(5)
-            continue
-        except ValueError:
-            print("Error parsing JSON response.")
-            continue
-
-        for topic in topics:
             file_path = f"data/links/{topic}/{year}/month{month}.txt"
             os.makedirs(os.path.dirname(file_path), exist_ok=True)
             with open(file_path, "w", encoding="utf-8") as file:
@@ -48,6 +47,6 @@ for i in range(amount_years):
                             continue
                         file.write(url + '\n')
 
-            print(f"Links for {topic} successfully saved in '{
-                  year}/month{month}.txt'.")
-            time.sleep(3)
+                print(f"Links for {topic} successfully saved in '{
+                    year}/month{month}.txt'.")
+                time.sleep(3)
