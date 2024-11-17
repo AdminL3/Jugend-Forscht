82e81682b1f6f14930823fad520a2788745b6f9c
AdminL3 <l-blu@outlook.de>
2024-11-17 18:26:16 +0100

    Crazy upgrades
    do not want to explain

--- Analysing/Wordcount/Get_extrema.py
+++ Analysing/Wordcount/Get_extrema.py
@@ -1,22 +1,43 @@
 import sqlite3
+from turtle import title
 
-topics = ["Politics", "World", "Opinion"]
-for i, topic in enumerate(topics):
-    print(topic)
 
+def get_title(date, topic):
+    parts = date.split("-")
+
+    topic = "Politics"
+    path = f"data/articles/{topic}/{parts[0]
+                                    }/month{parts[1]}/day{parts[2]}/{parts[3]}.txt"
+    with open(path, "r", encoding="utf-8") as file:
+        return (file.read().splitlines()[0])
+
+
+def get_extrema(topic):
+    text = ""
     connection = sqlite3.connect("Analysing\Wordcount\wordcount.db")
     cursor = connection.cursor()
 
     cursor.execute(f"SELECT * FROM {topic};")
     rows = cursor.fetchall()
 
-    with open("Analysing/Wordcount/new/wordcounts.txt", "a") as file:
-        file.write(f"Wordcount for {topic}:\n\n")
-        # Assuming row[0] is the day and row[1] is the word count
-        word_counts = [(row[0], row[1]) for row in rows]
-        word_counts.sort(key=lambda x: x[1], reverse=True)
+    text += f"{topic}:\nTop 5:\n\n"
+    text += ""
 
-        file.write("Top 5 days with most words:\n")
-        for day, count in word_counts[:5]:
-            file.write(f"Day: {day}, Word count: {count}\n")
-        file.write("\n")
+    word_counts = [(row[1], row[2], row[3]) for row in rows]
+    word_counts.sort(key=lambda x: x[2], reverse=True)
+
+    for element in word_counts[:5]:
+        identifier = f"{element[0]}-{element[1]}"
+        title = get_title(identifier, topic)
+        text += f"Date: {identifier}\nWord count: {
+            element[2]}\nTitle: {title}\n\n\n"
+    text += "\n"
+    return text
+
+
+topics = ["Politics", "World", "Opinion"]
+with open("Analysing/Wordcount/new/wordcounts.txt", "w") as file:
+    file.write("Wordcount analysis\n\n")
+with open("Analysing/Wordcount/new/wordcounts.txt", "a") as file:
+    for i, topic in enumerate(topics):
+        file.write(get_extrema(topic))
