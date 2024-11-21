49ea667162ea3cc4b6238c3ffc2775478bb55e09
AdminL3 <l-blu@outlook.de>
2024-11-21 13:32:38 +0100

    Added multiple plotting function

--- Analysing/Plotting.py
+++ Analysing/Plotting.py
@@ -1,6 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from pyparsing import col
 from sklearn.linear_model import LinearRegression
 
 
@@ -48,3 +49,47 @@ def graph(rows, column_names, name, title1, title2, drop_columns, color, color_r
     plt.savefig(output)
 
     plt.close()
+
+
+def multiple(all_rows, all_columns, name, all_titles, drop_columns, colors, colors_reg, regression, size, output):
+    for i, rows in enumerate(all_rows):
+        color = colors[i]
+
+        Dataframe = pd.DataFrame(
+            rows, columns=all_columns[i])
+
+        for column in drop_columns:
+            Dataframe = Dataframe.drop(columns=[f'{column}'])
+
+        Dataframe['date'] = pd.to_datetime(Dataframe['date'])
+        Dataframe.set_index('date', inplace=True)
+
+        plt.plot(Dataframe.index,
+                 Dataframe[f'{name}'], 'o', markersize=size, color=color)
+
+    if regression:
+        for i, rows in enumerate(all_rows):
+            Dataframe = pd.DataFrame(
+                rows, columns=all_columns[i])
+            for column in drop_columns:
+                Dataframe = Dataframe.drop(columns=[f'{column}'])
+
+            Dataframe['date'] = pd.to_datetime(Dataframe['date'])
+            Dataframe.set_index('date', inplace=True)
+
+            X = Dataframe.index.astype(np.int64).values.reshape(-1, 1)
+            y = Dataframe[f'{name}']
+            model = LinearRegression()
+            model.fit(X, y)
+            y_pred = model.predict(X)
+            plt.plot(Dataframe.index, y_pred, color=colors_reg[i])
+
+        legend1 = [f"Word Count for {topic}" for topic in all_titles]
+        legend2 = [f"Regression Line for {topic}" for topic in all_titles]
+        plt.xlabel("Date")
+        plt.ylabel(name.capitalize())
+        plt.legend(legend1 + legend2)
+        plt.title("Word Count Analysis")
+
+        plt.savefig(output)
+        plt.close()
--- Analysing/Wordcount/Graph Together.py
+++ Analysing/Wordcount/Graph Together.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from Analysing.Plotting import multiple
 
 connection = sqlite3.connect("Analysing\Wordcount\wordcount.db")
 cursor = connection.cursor()
@@ -11,46 +12,58 @@ cursor = connection.cursor()
 colors = ['#1f77b4', '#ff7f0e', "green"]
 colors_reg = ['blue', 'red', "black"]
 topics = ["Politics", "World", "Opinion"]
-
+all_rows = []
+all_columns = []
 for i, topic in enumerate(topics):
     cursor.execute(f"SELECT * FROM {topic};")
     rows = cursor.fetchall()
-
-    Dataframe = pd.DataFrame(
-        rows, columns=[column[0] for column in cursor.description])
-
-    Dataframe = Dataframe.drop(columns=['id'])
-    Dataframe['date'] = pd.to_datetime(Dataframe['date'])
-    Dataframe.set_index('date', inplace=True)
-
-    plt.plot(Dataframe.index,
-             Dataframe['wordcount'], 'o', markersize=1, color=colors[i])
+    all_rows.append(rows)
+    all_columns.append([column[0] for column in cursor.description])
+all_titles = ["Politics", "World", "Opinion"]
+drop_columns = ["id", "idx"]
+size = 2
+output = "Analysing\Wordcount\output\Together.png"
+name = "wordcount"
+regression = True
 
 
-for i, topic in enumerate(topics):
-    cursor.execute(f"SELECT * FROM {topic};")
-    rows = cursor.fetchall()
+multiple(all_rows, all_columns, name, all_titles, drop_columns,
+         colors, colors_reg, regression, size, output)
 
-    Dataframe = pd.DataFrame(
-        rows, columns=[column[0] for column in cursor.description])
+#     Dataframe = pd.DataFrame(
+#         rows, columns=[column[0] for column in cursor.description])
 
-    Dataframe = Dataframe.drop(columns=['id'])
-    Dataframe['date'] = pd.to_datetime(Dataframe['date'])
-    Dataframe.set_index('date', inplace=True)
+#     Dataframe = Dataframe.drop(columns=['id'])
+#     Dataframe['date'] = pd.to_datetime(Dataframe['date'])
+#     Dataframe.set_index('date', inplace=True)
 
-    X = Dataframe.index.astype(np.int64).values.reshape(-1, 1)
-    y = Dataframe['wordcount']
-    model = LinearRegression()
-    model.fit(X, y)
-    y_pred = model.predict(X)
-    plt.plot(Dataframe.index, y_pred, color=colors_reg[i])
+#     plt.plot(Dataframe.index,
+#              Dataframe['wordcount'], 'o', markersize=1, color=colors[i])
 
-legend1 = [f"Word Count for {topic}" for topic in topics]
-legend2 = [f"Regression Line for {topic}" for topic in topics]
-plt.xlabel("Date")
-plt.ylabel("Word Count")
-plt.legend(legend1 + legend2)
-plt.title("Word Count Analysis")
+# for i, topic in enumerate(topics):
+#     cursor.execute(f"SELECT * FROM {topic};")
+#     rows = cursor.fetchall()
 
-plt.savefig("Analysing\Wordcount\output\Together.png")
-plt.close()
+#     Dataframe = pd.DataFrame(
+#         rows, columns=[column[0] for column in cursor.description])
+
+#     Dataframe = Dataframe.drop(columns=['id'])
+#     Dataframe['date'] = pd.to_datetime(Dataframe['date'])
+#     Dataframe.set_index('date', inplace=True)
+
+#     X = Dataframe.index.astype(np.int64).values.reshape(-1, 1)
+#     y = Dataframe['wordcount']
+#     model = LinearRegression()
+#     model.fit(X, y)
+#     y_pred = model.predict(X)
+#     plt.plot(Dataframe.index, y_pred, color=colors_reg[i])
+
+# legend1 = [f"Word Count for {topic}" for topic in topics]
+# legend2 = [f"Regression Line for {topic}" for topic in topics]
+# plt.xlabel("Date")
+# plt.ylabel("Word Count")
+# plt.legend(legend1 + legend2)
+# plt.title("Word Count Analysis")
+
+# plt.savefig("Analysing\Wordcount\output\Together.png")
+# plt.close()
