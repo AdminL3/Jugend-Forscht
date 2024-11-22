bae6733c3141a8dfede2600c40a79d496a2a7fdc
AdminL3 <l-blu@outlook.de>
2024-11-22 17:54:48 +0100

    Updated settings for Link parsing

--- data-collection/Guardian/Parse_links.py
+++ data-collection/Guardian/Parse_links.py
@@ -2,11 +2,14 @@ import os
 
 start_year = 2020
 amount_years = 2
+start_month = 1
+amount_months = 10
 topics = ["politics", "world", "opinion"]
 titles = ["Politics", "World", "commentisfree"]
 for i in range(amount_years):
     year = start_year + i
-    for month in range(1, 13):
+    for j in range(amount_months):
+        month = start_month + j
         for t, topic in enumerate(topics):
             counter = 0
 
@@ -14,11 +17,15 @@ for i in range(amount_years):
             file_name = f"month{month:02}.txt"
             if os.path.exists(file_path + file_name):
                 with open(file_path + file_name, "r", encoding="utf-8") as file:
-                    all_links = file.splitlines()
+                    all_links = file.read().splitlines()
+                os.makedirs(file_path.replace("all", topic), exist_ok=True)
                 with open(file_path.replace("all", topic) + file_name, "w", encoding="utf-8") as file:
                     for link in all_links:
                         if (titles[t].lower() in link.lower()):
                             file.write(link + "\n")
-                            counter += 1
+                            parts = link.split("/")
+                            if titles[t].lower() == parts[3] or titles[t].lower() == parts[4]:
+                                file.write(link + "\n")
+                                counter += 1
 
                 print(f"Saved {counter} links for {topic}, {year}-{month:02}.")
