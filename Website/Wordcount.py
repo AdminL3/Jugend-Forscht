64c148d98f193b36a6c5788c21625bbbc22e101a
AdminL3 <l-blu@outlook.de>
2024-12-07 11:01:11 +0100

    Added a button to generate data

--- Website/Wordcount.py
+++ Website/Wordcount.py
@@ -126,66 +126,73 @@ st.markdown(
 
 st.divider()
 
-# Filter data based on selected year and month range
-for i in range(amount_of_plots):
-    all_data[i][0] = all_data[i][0][
-        (all_data[i][0]['date'].dt.year >= year_range[0]) &
-        (all_data[i][0]['date'].dt.year <= year_range[1]) &
-        (all_data[i][0]['date'].dt.month >= month_range[0]) &
-        (all_data[i][0]['date'].dt.month <= month_range[1])
-    ]
-
-    # Format the date column to exclude time
-    all_data[i][0]['date'] = all_data[i][0]['date'].dt.date
-
-
 # Scatter plot
 # title
 st.subheader("Scatter Plot:")
 st.write(f"{year_range[0]} - {month_range[0]} to {
-         month_range[1]}" if one_year else f"{year_range[0]} to {year_range[1]}")
+    month_range[1]}" if one_year else f"{year_range[0]} to {year_range[1]}")
+if st.button("Generate Graph"):
+    with st.spinner("Generating Graph..."):
+        try:
+            st.write(
+                "Note: Click on elements in the Legend to make them invisible.")
+            # Filter data based on selected year and month range
+            for i in range(amount_of_plots):
+                all_data[i][0] = all_data[i][0][
+                    (all_data[i][0]['date'].dt.year >= year_range[0]) &
+                    (all_data[i][0]['date'].dt.year <= year_range[1]) &
+                    (all_data[i][0]['date'].dt.month >= month_range[0]) &
+                    (all_data[i][0]['date'].dt.month <= month_range[1])
+                ]
 
-# Create scatter plot
-fig = go.Figure()
+                # Format the date column to exclude time
+                all_data[i][0]['date'] = all_data[i][0]['date'].dt.date
 
-# Add scatter points
-for i in range(amount_of_plots):
-    fig.add_trace(go.Scatter(
-        x=all_data[i][0]['date'],
-        y=all_data[i][0]['wordcount'],
-        mode='markers',
-        name='Article Count: ' + all_data[i][1] + " - " + all_data[i][2],
-        marker=dict(color=all_data[i][3], size=5)
-    ))
+            # Create scatter plot
+            fig = go.Figure()
 
-for i in range(amount_of_plots):
-    # Linear Regression
-    model = LinearRegression()
-    X = pd.to_numeric(all_data[i][0]['date'].map(
-        datetime.datetime.toordinal)).values.reshape(-1, 1)
-    y = all_data[i][0]['wordcount'].values
+            # Add scatter points
+            for i in range(amount_of_plots):
+                fig.add_trace(go.Scatter(
+                    x=all_data[i][0]['date'],
+                    y=all_data[i][0]['wordcount'],
+                    mode='markers',
+                    name='Article Count: ' +
+                    all_data[i][1] + " - " + all_data[i][2],
+                    marker=dict(color=all_data[i][3], size=5)
+                ))
 
-    model.fit(X, y)
-    y_pred = model.predict(X)
+            for i in range(amount_of_plots):
+                # Linear Regression
+                model = LinearRegression()
+                X = pd.to_numeric(all_data[i][0]['date'].map(
+                    datetime.datetime.toordinal)).values.reshape(-1, 1)
+                y = all_data[i][0]['wordcount'].values
 
-    # Add regression line
-    fig.add_trace(go.Scatter(
-        x=all_data[i][0]['date'],
-        y=y_pred,
-        mode='lines',
-        name='Regression Line: ' + all_data[i][1] + " - " + all_data[i][2],
-        line=dict(color=all_data[i][4], width=2)
-    ))
+                model.fit(X, y)
+                y_pred = model.predict(X)
 
-# Update layout
-fig.update_layout(
-    title='Word Count Development',
-    xaxis_title='Date',
-    yaxis_title='Word Count',
-    dragmode="pan"
-)
+                # Add regression line
+                fig.add_trace(go.Scatter(
+                    x=all_data[i][0]['date'],
+                    y=y_pred,
+                    mode='lines',
+                    name='Regression Line: ' +
+                    all_data[i][1] + " - " + all_data[i][2],
+                    line=dict(color=all_data[i][4], width=2)
+                ))
 
-st.plotly_chart(fig, use_container_width=True)
+            # Update layout
+            fig.update_layout(
+                title='Word Count Development',
+                xaxis_title='Date',
+                yaxis_title='Word Count',
+                dragmode="pan"
+            )
+
+            st.plotly_chart(fig, use_container_width=True)
+        except Exception as e:
+            st.error("**Error:**  \nAn error occurred while generating the graph.  \nIt looks like the range you selected doesn't have any data.  \nCurrenly available data ranges are 2010 - 2010 and 2020 - 2022.")
 
 st.divider()
 
@@ -247,37 +254,36 @@ for i in range(amount_of_plots):
     # Format the Date column to exclude time
     filtered_top_data['Date'] = filtered_top_data['Date'].dt.date
 
-    # Add a Title column using the get_title function
     st.write(f"Top Articles for {selected_news} - {selected_topic}")
+    if st.button("Generate Table", key=i+4*amount_of_plots):
+        if selected_news == "Both" or selected_topic == "All" or selected_topic == "Neutral":
+            st. write(
+                "Currently, we only support fetching titles for a single news source and a single topic.")
+        else:
+            with st.spinner("Fetching Articles..."):
+                conn2 = sqlite3.connect(f'Database/Titles/{selected_news}.db')
+                cursor2 = conn2.cursor()
+                titles = []
+                for _, row in filtered_top_data.iterrows():
+                    date = row['Date'].strftime("%Y-%m-%d")
+                    cursor2.execute(
+                        f"SELECT title FROM {selected_topic.lower()} WHERE date=? AND idx=?", (date, row['Day Index']))
+                    title = cursor2.fetchone()[0]
+                    titles.append(title)
+                filtered_top_data['Title'] = titles
 
-    if selected_news == "Both" or selected_topic == "All" or selected_topic == "Neutral":
-        st. write(
-            "Unable to display article titles. Please select one news source and topic to view titles.")
-    else:
-        with st.spinner("Fetching Articles..."):
-            conn2 = sqlite3.connect(f'Database/Titles/{selected_news}.db')
-            cursor2 = conn2.cursor()
-            titles = []
-            for _, row in filtered_top_data.iterrows():
-                date = row['Date'].strftime("%Y-%m-%d")
-                cursor2.execute(
-                    f"SELECT title FROM {selected_topic.lower()} WHERE date=? AND idx=?", (date, row['Day Index']))
-                title = cursor2.fetchone()[0]
-                titles.append(title)
-            filtered_top_data['Title'] = titles
-
-    filtered_top_data = filtered_top_data.head(10)
-    styled_dataframe = filtered_top_data.style.applymap(
-        lambda x: 'color: yellow', subset=['Wordcount'])
-    st.dataframe(
-        styled_dataframe,
-        use_container_width=True,
-        hide_index=True,
-        column_config={
-            "ID": st.column_config.Column(width=-100),
-            "Wordcount": st.column_config.Column(width=-100),
-            "Date": st.column_config.Column(width=-100),
-            "Day Index": st.column_config.Column(width=-100),
-            "Title": st.column_config.Column(width=600)
-        }
-    )
+        filtered_top_data = filtered_top_data.head(10)
+        styled_dataframe = filtered_top_data.style.applymap(
+            lambda x: 'color: yellow', subset=['Wordcount'])
+        st.dataframe(
+            styled_dataframe,
+            use_container_width=True,
+            hide_index=True,
+            column_config={
+                "ID": st.column_config.Column(width=-100),
+                "Wordcount": st.column_config.Column(width=-100),
+                "Date": st.column_config.Column(width=-100),
+                "Day Index": st.column_config.Column(width=-100),
+                "Title": st.column_config.Column(width=600)
+            }
+        )
