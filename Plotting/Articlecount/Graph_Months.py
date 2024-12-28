a7f55279e5f708e8c95755b15d63d59cf8352e49
AdminL3 <l-blu@outlook.de>
2024-12-28 19:07:57 +0100

    Graphing the articlecount

--- Plotting/Articlecount/Graph_Months.py
+++ Plotting/Articlecount/Graph_Months.py
@@ -1,48 +1,25 @@
 import os
 import sqlite3
-from Plotting.Plotting import graph, multiple
+from Plotting.Plotting import graph
 
+for news in ["NYT", "Guardian"]:
+    connection = sqlite3.connect(f"Database/Articlecount/{news}.db")
+    cursor = connection.cursor()
 
-connection = sqlite3.connect("Database/Articlecount/articlecount.db")
-cursor = connection.cursor()
+    colors = ['#1f77b4', '#ff7f0e', "green"]
+    colors_reg = ['blue', 'red', "black"]
+    topics = ["Politics", "World", "Opinion"]
+    for i in range(len(topics)):
+        topic = topics[i]
+        color = colors[i]
+        regression_color = colors_reg[i]
 
-colors = ['#1f77b4', '#ff7f0e', "green"]
-colors_reg = ['blue', 'red', "black"]
-topics = ["Politics", "World", "Opinion"]
-for i in range(len(topics)):
-    topic = topics[i]
-    color = colors[i]
-    regression_color = colors_reg[i]
+        cursor.execute(f"SELECT * FROM {topic}")
+        rows = cursor.fetchall()
 
-    cursor.execute(f"SELECT * FROM monthly_totals WHERE topic_id = {i}")
-    rows = cursor.fetchall()
+        columns = [column[0] for column in cursor.description]
+        output = f"Output/Articlecount/Month/{topic}.png"
+        os.makedirs(os.path.dirname(output), exist_ok=True)
 
-    columns = [column[0] for column in cursor.description]
-    output = f"Output/Articlecount/Month/{topic}.png"
-    os.makedirs(os.path.dirname(output), exist_ok=True)
-
-    graph(rows, columns, "total_count", f"{topic} Articlecount", f"Articlecount of {topic}", ["id", "topic_id"],
-          color, regression_color, True, 2, output)
-
-
-# Together
-
-
-all_rows = []
-all_columns = []
-for i, topic in enumerate(topics):
-    cursor.execute(f"SELECT * FROM monthly_totals WHERE topic_id = {i}")
-    rows = cursor.fetchall()
-    all_rows.append(rows)
-    all_columns.append([column[0] for column in cursor.description])
-all_titles = ["Politics", "World", "Opinion"]
-drop_columns = ["id", "topic_id"]
-size = 2
-
-output = "Output/Articlecount/Month/Together.png"
-os.makedirs(os.path.dirname(output), exist_ok=True)
-name = "total_count"
-regression = True
-
-multiple(all_rows, all_columns, name, all_titles, drop_columns,
-         colors, colors_reg, regression, size, output)
+        graph(rows, columns, "count", f"{topic} Articlecount", f"Articlecount of {topic}", ["id"],
+              color, regression_color, True, 2, output)
