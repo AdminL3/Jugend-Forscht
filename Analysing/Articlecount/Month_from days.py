ac67e22d60c62f68a45e88255602c71b5638a038
AdminL3 <l-blu@outlook.de>
2024-12-28 19:08:52 +0100

    Getting the Month Articlecount

---
+++ Analysing/Articlecount/Month_from days.py
@@ -0,0 +1,38 @@
+import os
+import sqlite3
+
+topics = ["politics", "world", "opinion"]
+
+news = ["NYT", "Guardian"]
+for n in news:
+    conn = sqlite3.connect(f"Database/Articlecount/Days/{n}.db")
+    cursor = conn.cursor()
+
+    for i, topic in enumerate(topics):
+        print(f"Processing topic: {topic}")
+        root_dir = f"data/{n}/articles/{topic}"
+
+        count = 0
+        years = [f for f in os.listdir(
+            root_dir) if os.path.isdir(os.path.join(root_dir, f))]
+        for year in years:
+            year_dir = os.path.join(root_dir, year)
+            months = [f for f in os.listdir(
+                year_dir) if os.path.isdir(os.path.join(year_dir, f))]
+            for month in months:
+                month_dir = os.path.join(year_dir, month)
+                days = [f for f in os.listdir(
+                    month_dir) if os.path.isdir(os.path.join(month_dir, f))]
+                for day in days:
+                    day_dir = os.path.join(month_dir, day)
+                    count = len(os.listdir(day_dir))
+
+                    month_n = int(month.split("month")[1])
+                    day_n = int(day.split("day")[1])
+                    cursor.execute(f"""
+                    INSERT INTO {topic} (year, month, day, count)
+                    VALUES ({year}, {month_n}, {day_n}, {count})
+                    """)
+                    print(f"Inserted {count} articles for {
+                          year}-{month}-{day}")
+                    conn.commit()
