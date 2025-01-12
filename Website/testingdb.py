59b5df8d4d19078ee5b7c860f7638319bf06e12c
AdminL3 <l-blu@outlook.de>
2025-01-12 10:48:42 +0100

    Trying to use database on live streamlit application

---
+++ Website/testingdb.py
@@ -0,0 +1,12 @@
+import sqlite3
+import pandas as pd
+import streamlit as st
+
+# SQLite-Datenbank aus einem GitHub-Repo laden
+db_url = "https://raw.githubusercontent.com/AdminL3/Jugend-Forscht/main/Database/Wordcount/Guardian.db"
+conn = sqlite3.connect("database.db")
+
+# Beispielabfrage
+query = "SELECT * FROM users"
+df = pd.read_sql_query(query, conn)
+st.dataframe(df)
